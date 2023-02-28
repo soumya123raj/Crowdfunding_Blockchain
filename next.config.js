666f3f40f83aaf50be0e780bbeb743e120ae379f
soumya123raj <78701991+soumya123raj@users.noreply.github.com>
2023-02-28 23:01:15 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  image: {
+    domains: ['crowdfunding4.infura-ipfs.io']
+  }
+}
+
+module.exports = nextConfig
